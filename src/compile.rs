7303ba42c5d6f068008fda1c25adf4b970884e64
funera1 <funeral7776@gmail.com>
2024-11-22 08:02:52 +0900

    Introduce WasmType

--- src/compile.rs
+++ src/compile.rs
@@ -12,21 +12,43 @@ pub struct FastBytecodeFunction<'a> {
     // func_body: &'a FunctionBody<'a>,
 }
 
-#[derive(Clone)]
+
+// const BYTE_U8: u8 = 1;
+// const BYTE_U32: u8 = 4;
+// const BYTE_U64: u8 = 8;
+// const BYTE_U128: u8 = 16;
+
+#[derive(Clone, Copy)]
 enum WasmType {
-    Bit32,
-    Bit64,
+    Any = 0,
+    ByteU8 = 1,
+    ByteU32 = 4,
+    ByteU64 = 8,
+    ByteU128 = 16,
 }
 
-const BYTE_U8: u8 = 1;
-const BYTE_U32: u8 = 4;
-const BYTE_U64: u8 = 8;
-const BYTE_U128: u8 = 16;
+fn valtype_to_wasmtype(valtype: ValType) -> WasmType {
+    match valtype {
+        ValType::I32 | ValType::F32 => return WasmType::ByteU32,
+        ValType::I64 | ValType::F64 => return WasmType::ByteU64,
+        // TODO: refはBYTE_U128じゃなさそう
+        ValType::V128 | ValType::Ref(_)=> return WasmType::ByteU128,
+    }
+}
+
+fn u8_to_wasmtype(num: u8) -> WasmType {
+    match num {
+        4 => return WasmType::ByteU32,
+        8 => return WasmType::ByteU64,
+        16 => return WasmType::ByteU128,
+        _ => return WasmType::Any,
+    }
+}
 
 #[derive(Clone)]
 pub struct OpType {
-    input: Vec<u8>,
-    output: Vec<u8>,
+    input: Vec<WasmType>,
+    output: Vec<WasmType>,
 }
 
 #[derive(Clone)]
@@ -56,7 +78,7 @@ impl<'a> FastBytecodeFunction<'a> {
         return Ok(compiled_funcs);
     }
 
-    fn emit_label(codepos: &CodePos<'a>, input: Vec<u8> , output: Vec<u8>) -> FastCodePos<'a> {
+    fn emit_label(codepos: &CodePos<'a>, input: Vec<WasmType> , output: Vec<WasmType>) -> FastCodePos<'a> {
         let f = FastCodePos {
             opcode: codepos.opcode.clone(),
             optype: OpType { input: input, output: output},
@@ -68,7 +90,7 @@ impl<'a> FastBytecodeFunction<'a> {
 
     pub fn compile_fast_bytecode(module: &Module, func: &'a BytecodeFunction) {
         // 仮スタック
-        let mut stack = Vec::new();
+        let mut stack: Vec<WasmType> = Vec::new();
         let mut fast_bytecode: Vec<FastCodePos> = Vec::new();
 
         for codepos in &func.codes {
@@ -114,7 +136,7 @@ impl<'a> FastBytecodeFunction<'a> {
                 Operator::LocalGet{ local_index } => {
                     // skip_label
                     // local_indexの型をstackにpushする
-                    stack.push(func.locals[local_index as usize]);
+                    stack.push(u8_to_wasmtype(func.locals[local_index as usize]));
                 }
                 Operator::LocalSet{ .. } => {
                     // TODO: local.getをskipすることで発生するごにょごにょを処理しないといけない
@@ -123,166 +145,119 @@ impl<'a> FastBytecodeFunction<'a> {
                     // TODO: local.getをskipすることで発生するごにょごにょを処理しないといけない
                 }
                 Operator::GlobalGet{ global_index } => {
-                    let operand: Vec<u8>;
-                    match module.globals[global_index as usize].content_type {
-                        ValType::I32 | ValType::F32 => operand = vec![BYTE_U32],
-                        ValType::I64 | ValType::F64 => operand = vec![BYTE_U64],
-                        // TODO: refはBYTE_U128じゃなさそう
-                        ValType::V128 | ValType::Ref(_)=> operand = vec![BYTE_U128],
-                    }
-                    fast_bytecode.push(Self::emit_label(codepos, vec![], operand));
+                    fast_bytecode.push(
+                        Self::emit_label(codepos, vec![], 
+                            vec![valtype_to_wasmtype(module.globals[global_index as usize].content_type)]));
                 }
                 Operator::GlobalSet{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![0], vec![]));
+                    fast_bytecode.push(Self::emit_label(codepos, vec![WasmType::Any], vec![]));
                 }
                 Operator::TableGet{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![0], vec![]));
+                    fast_bytecode.push(Self::emit_label(codepos, vec![WasmType::Any], vec![]));
                 }
                 Operator::TableSet{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 Operator::I32Load{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Load{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32Load{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64Load{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32Load8S{ .. } | Operator::I32Load8U{ .. } | Operator::I32Load16S{ .. } | Operator::I32Load16U{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Load8S{ .. } | Operator::I64Load8U{ .. } | Operator::I64Load16S{ .. } | Operator::I64Load16U{ .. } | Operator::I64Load32S{ .. } | Operator::I64Load32U{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32Store{ .. } | Operator::I64Store{ .. } | Operator::F32Store{ .. } | Operator::F64Store{ .. } 
                 | Operator::I32Store8{ .. } | Operator::I32Store16 { .. } | Operator::I64Store8{ .. } | Operator::I64Store16 { .. } | Operator::I64Store32{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::MemorySize{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::MemoryGrow{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 Operator::I32Const{ .. } | Operator::F32Const{ .. } => {
                     // skip_label
                     // TODO: magic numberをやめる
-                    stack.push(BYTE_U32);
+                    stack.push(WasmType::ByteU32);
                 }
                 Operator::I64Const{ .. } | Operator::F64Const{ .. } => {
                     // skip_label
                     // TODO: magic numberをやめる
-                    stack.push(BYTE_U64);
+                    stack.push(WasmType::ByteU64);
                 }
 
                 Operator::I32Eqz{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32Eq | Operator::I32Ne | Operator::I32LtS | Operator::I32LtU | Operator::I32GtS | Operator::I32GtU
                 | Operator::I32LeS | Operator::I32LeU | Operator::I32GeS | Operator::I32GeU => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Eqz{ .. } => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Eq | Operator::I64Ne | Operator::I64LtS | Operator::I64LtU | Operator::I64GtS | Operator::I64GtU
                 | Operator::I64LeS | Operator::I64LeU | Operator::I64GeS | Operator::I64GeU => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32Eq | Operator::F32Ne | Operator::F32Lt | Operator::F32Gt | Operator::F32Le | Operator::F32Ge => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64Eq | Operator::F64Ne | Operator::F64Lt | Operator::F64Gt | Operator::F64Le | Operator::F64Ge => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 Operator::I32Clz | Operator::I32Ctz | Operator::I32Popcnt => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32Add | Operator::I32Sub | Operator::I32Mul | Operator::I32DivS | Operator::I32DivU | Operator::I32RemS | Operator::I32RemU => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32And | Operator::I32Or | Operator::I32Xor | Operator::I32Shl | Operator::I32ShrS | Operator::I32ShrU | Operator::I32Rotl | Operator::I32Rotr => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Clz | Operator::I64Ctz | Operator::I64Popcnt => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64Add | Operator::I64Sub | Operator::I64Mul | Operator::I64DivS | Operator::I64DivU | Operator::I64RemS | Operator::I64RemU => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64And | Operator::I64Or | Operator::I64Xor | Operator::I64Shl | Operator::I64ShrS | Operator::I64ShrU | Operator::I64Rotl | Operator::I64Rotr => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 Operator::F32Abs | Operator::F32Neg | Operator::F32Ceil | Operator::F32Floor | Operator::F32Trunc | Operator::F32Nearest | Operator::F32Sqrt => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32Add | Operator::F32Sub | Operator::F32Mul | Operator::F32Div | Operator::F32Min | Operator::F32Max | Operator::F32Copysign => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64Abs | Operator::F64Neg | Operator::F64Ceil | Operator::F64Floor | Operator::F64Trunc | Operator::F64Nearest | Operator::F64Sqrt => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64Add | Operator::F64Sub | Operator::F64Mul | Operator::F64Div | Operator::F64Min | Operator::F64Max | Operator::F64Copysign => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 Operator::I32WrapI64 => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32TruncF32S | Operator::I32TruncF32U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32TruncF64S | Operator::I32TruncF64U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64ExtendI32S | Operator::I64ExtendI32U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64TruncF32S | Operator::I64TruncF32U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I64TruncF64S | Operator::I64TruncF64U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32ConvertI32S | Operator::F32ConvertI32U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32ConvertI64S | Operator::F32ConvertI64U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F32DemoteF64 => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64ConvertI32S | Operator::F64ConvertI32U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64ConvertI64S | Operator::F64ConvertI64U => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::F64PromoteF32 => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32ReinterpretF32 | Operator::I64ReinterpretF64 | Operator::F32ReinterpretI32 | Operator::F64ReinterpretI64 => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::I32Extend8S | Operator::I32Extend16S | Operator::I64Extend8S | Operator::I64Extend16S | Operator::I64Extend32S => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 
                 Operator::MemoryCopy{..} => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
                 Operator::MemoryFill{..} => {
-                    fast_bytecode.push(Self::emit_label(codepos, vec![BYTE_U32]));
                 }
 
                 ref _other => {
