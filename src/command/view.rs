1b4ee7fb2f2872720ded44031a28ba70c96aedb4
funera1 <funeral7776@gmail.com>
2025-06-29 13:04:30 +0900

    Separete view_v1_format to a parse function and a pritn function

--- src/command/view.rs
+++ src/command/view.rs
@@ -100,19 +100,18 @@ fn read_u8(cursor: &mut usize, data: &[u8]) -> Result<u8> {
     Ok(byte)
 }
 
-/// Parse and display a v1 format binary file
-/// This implements the equivalent functionality of the Python parser script
-pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
-    let data = fs::read(&path)?;
+/// Parse a v1 format binary file and return the parsed data
+fn parse_v1_format(path: &Utf8PathBuf) -> Result<V1FormatData> {
+    let data = fs::read(path)?;
     let mut cursor = 0;
 
     // Read entry function index
     let entry_fidx = read_u32(&mut cursor, &data)?;
-    
+
     // Read return address
     let return_fidx = read_u32(&mut cursor, &data)?;
     let return_offset = read_u32(&mut cursor, &data)?;
-    
+
     // Read type stack
     let type_stack_size = read_u32(&mut cursor, &data)?;
     let mut type_stack = Vec::new();
@@ -126,7 +125,7 @@ pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
     for i in 0..type_stack_size {
         let typ = type_stack[i as usize];
         let byte_count = (typ as usize) * 4;
-        
+
         // Read bytes for this value
         let mut value_bytes = Vec::new();
         for _ in 0..byte_count {
@@ -136,7 +135,7 @@ pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
             value_bytes.push(data[cursor]);
             cursor += 1;
         }
-        
+
         // Convert bytes to value (equivalent to Python's to_int function)
         let value = bytes_to_int(&value_bytes)?;
         value_stack.push(value);
@@ -145,7 +144,7 @@ pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
     // Read label stack
     let label_stack_size = read_u32(&mut cursor, &data)?;
     let mut label_stack = Vec::new();
-    
+
     for _ in 0..label_stack_size {
         let label = Label {
             begin_addr: read_u32_or_zero(&mut cursor, &data),
@@ -158,31 +157,37 @@ pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
         label_stack.push(label);
     }
 
+    Ok(V1FormatData {
+        entry_func_idx: entry_fidx,
+        return_address: (return_fidx, return_offset),
+        stack_size: type_stack_size,
+        type_stack,
+        value_stack,
+        label_stack_size,
+        label_stack,
+    })
+}
+
+/// Parse and display a v1 format binary file
+/// This implements the equivalent functionality of the Python parser script
+pub fn view_v1_format(path: Utf8PathBuf, json_output: bool) -> Result<()> {
+    let parsed_data = parse_v1_format(&path)?;
+
     // Output results in requested format
     if json_output {
         // JSON output
-        let output = V1FormatData {
-            entry_func_idx: entry_fidx,
-            return_address: (return_fidx, return_offset),
-            stack_size: type_stack_size,
-            type_stack,
-            value_stack,
-            label_stack_size,
-            label_stack,
-        };
-        
-        let json = serde_json::to_string_pretty(&output)?;
+        let json = serde_json::to_string_pretty(&parsed_data)?;
         println!("{}", json);
     } else {
         // Original format output
-        println!("EntryFuncIdx: {}", entry_fidx);
-        println!("ReturnAddress: {}, {}", return_fidx, return_offset);
-        println!("StackSize: {}", type_stack_size);
-        println!("TypeStack: {:?}", type_stack);
-        println!("ValueStack: {:?}", value_stack);
-        println!("LabelStackSize: {}", label_stack_size);
+        println!("EntryFuncIdx: {}", parsed_data.entry_func_idx);
+        println!("ReturnAddress: {}, {}", parsed_data.return_address.0, parsed_data.return_address.1);
+        println!("StackSize: {}", parsed_data.stack_size);
+        println!("TypeStack: {:?}", parsed_data.type_stack);
+        println!("ValueStack: {:?}", parsed_data.value_stack);
+        println!("LabelStackSize: {}", parsed_data.label_stack_size);
         println!("LabelStack: [");
-        for label in &label_stack {
+        for label in &parsed_data.label_stack {
             println!("\t{}", label);
         }
         println!("]");
@@ -317,5 +322,51 @@ fn parse_label_stack(cursor: &mut usize, data: &[u8]) -> Result<serde_json::Valu
         "targets": targets,
         "stack_pointers": stack_pointers,
         "cell_nums": cell_nums
+    }))
+}
+
+/// Parse and display multiple v1 format binary files
+/// This function aggregates multiple v1 stack files into a unified JSON format
+pub fn view_v1_format_multiple(paths: Vec<Utf8PathBuf>, json_output: bool) -> Result<()> {
+    let mut call_stack = Vec::new();
+
+    for (frame_index, path) in paths.iter().enumerate() {
+        let data = fs::read(path)?;
+        let frame = parse_v1_frame(&data, frame_index)?;
+        call_stack.push(frame);
+    }
+
+    if json_output {
+        let output = serde_json::json!({ "call_stack": call_stack });
+        println!("{}", serde_json::to_string_pretty(&output)?);
+    } else {
+        for frame in call_stack {
+            println!("{:?}", frame);
+        }
+    }
+
+    Ok(())
+}
+
+/// Parse a single v1 frame from binary data
+fn parse_v1_frame(data: &[u8], frame_index: usize) -> Result<serde_json::Value> {
+    let mut cursor = 0;
+
+    let pc = {
+        let fidx = read_u32(&mut cursor, data)?;
+        let offset = read_u32(&mut cursor, data)?;
+        serde_json::json!({ "fidx": fidx, "offset": offset })
+    };
+
+    let locals = parse_typed_array(&mut cursor, data)?;
+    let value_stack = parse_typed_array(&mut cursor, data)?;
+    let label_stack = parse_label_stack(&mut cursor, data)?;
+
+    Ok(serde_json::json!({
+        "frame_index": frame_index,
+        "pc": pc,
+        "locals": locals,
+        "value_stack": value_stack,
+        "label_stack": label_stack
     }))
 }
